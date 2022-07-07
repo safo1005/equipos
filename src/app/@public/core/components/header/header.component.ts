2045927d32c406e9fd448221d8b54e80296c9063
safo1005 <marthaquintanasarabia@gmail.com>
2022-07-06 22:03:38 -0500

    Configuración del sidebar terminado

---
+++ src/app/@public/core/components/header/header.component.ts
@@ -0,0 +1,21 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent {
+  toggledValue = true;
+  @Output() toggleChange = new EventEmitter<boolean>();
+
+  toggled() {
+    if (this.toggledValue === undefined)
+    {
+      this.toggledValue = true;
+    }
+    this.toggledValue = !this.toggledValue;
+    console.log(this.toggledValue);
+    this.toggleChange.emit(this.toggledValue);
+  }
+}
--- src/app/@public/pages/public.module.ts
+++ src/app/@public/pages/public.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { PublicRoutingModule } from './public-routing.module';
 import { PublicComponent } from './public.component';
+import { HeaderComponent } from '../core/components/header/header.component';
+import { TitleComponent } from '../core/components/title/title.component';
+import { SidebarComponent } from '../core/components/sidebar/sidebar.component';
 
 
 @NgModule({
   declarations: [
-    PublicComponent
+    PublicComponent,
+    HeaderComponent,
+    TitleComponent,
+    SidebarComponent
   ],
   imports: [
     CommonModule,
